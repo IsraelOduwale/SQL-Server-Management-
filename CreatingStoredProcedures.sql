79d942d1e4b701d4d68e5e8892cfadd698257b82
LondonBoyod <106557777+LondonBoyod@users.noreply.github.com>
2024-08-06 05:09:08 +0100

    Add files via upload

---
+++ CreatingStoredProcedures.sql
@@ -0,0 +1,51 @@
+use SQLTraining
+
+--STored Procedure using World table
+create procedure spWorld
+as
+begin
+select * from World
+end
+
+exec spWorld
+
+--STored Procedure using Games table
+create procedure spGames
+as
+begin
+select * from Games
+end
+
+exec spGames
+
+
+--STored Procedure using ComapnyInfo table
+use SQLTrainingCase
+
+create procedure spCompanyInfo
+as
+begin
+select * from CompanyInfo
+end
+
+exec spCompanyInfo
+
+
+--STored Procedure using EmployeeInfo table
+create procedure spEmployeeInfo
+as
+begin
+select * from EmployeeInfo
+end
+
+exec spEmployeeInfo
+
+
+--STored Procedure using Business table
+create procedure spBusiness
+as
+begin
+select * from Business
+end
+
+exec spBusiness
